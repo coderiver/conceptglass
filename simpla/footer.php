65332aa067199338a696ffddb5bad82d517503ba
moroz <morozarturv@gmail.com>
2015-09-28 10:54:35 +0300

    small changes

---
+++ simpla/footer.php
@@ -0,0 +1,12 @@
+</div>
+<!-- load scripts -->
+<script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/js/lib/head.js"></script> 
+<script> 
+	head.js("<?php bloginfo('template_directory'); ?>/js/lib/jquery.js",
+			"<?php bloginfo('template_directory'); ?>/js/lib/slick.min.js",
+			"<?php bloginfo('template_directory'); ?>/js/lib/jquery.fullPage.min.js",
+			"<?php bloginfo('template_directory'); ?>/js/common.js"
+	);
+</script>
+</body>
+</html>
---
+++ simpla/header.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>CONCEPT.GLASS</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="format-detection" content="telephone=no">
+	<link rel="stylesheet" media="all" href="<?php bloginfo('template_directory'); ?>/css/screen.css">
+</head>
+<body>
+<!-- wrapper -->
+<div class="wrapper">

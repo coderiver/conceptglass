fc0df9be9d9b6b28bbebbe8895ef756faac92b2d
moroz <arturmorozv@gmail.com>
2015-09-30 12:56:41 +0300

    some updates

--- app/js/common.js
+++ app/js/common.js
@@ -12,7 +12,7 @@ head.ready(function() {
 
 	// types
 	(function () {
-		var fp      = $('.js-fp');
+		var fp = $('.js-fp');
 		if (fp.length) {
 			fp.fullpage({
 				sectionSelector: '.js-fp-section',
@@ -27,13 +27,26 @@ head.ready(function() {
 	(function () {
 		var brands = $('.js-brands');
 		if (brands.length) {
+			var client      = brands.data('client'),
+				activeSlide = $('.js-brands-item.' + client),
+				index       = activeSlide.index();
+			activeSlide.addClass('is-active');
 			brands.slick({
+				initialSlide: index,
 				centerMode: true,
 				variableWidth: true,
 				infinity: true,
 				swipe: false
 			});
 		};
+	}());
+
+	// go back
+	(function () {
+		$('.js-back').on('click', function () {
+			window.history.back();
+			return false;
+		});
 	}());
 
 	// go top
